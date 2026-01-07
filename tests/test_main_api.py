5c8f12a598f81b1c0e61662719488d4c23d22983
mcormack <martincormack5@gmail.com>
2026-01-07 18:27:22 +0100

    Bugfix

--- tests/test_main_api.py
+++ tests/test_main_api.py
@@ -4,6 +4,6 @@ from app.main_api import app
 client = TestClient(app)
 
 def test_read_main():
-    response = client.get("/")
+    response = client.get("/say_hello")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
